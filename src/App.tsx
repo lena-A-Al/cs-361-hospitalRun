183f66c2bd339bfb9a1d162ee0ba22d25b999ed8
lena <lenaal@lenas-MacBook-Pro.local>
2024-11-03 20:58:06 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Main } from './core/Main';
+import { Home } from './components/Home';
+import { PatientInfo } from './components/PatientInfo';
+import { appPathNames } from './common/utils/NavUtil';
+import { Appointments } from './components/Appointments';
+import { PrimaryCareDoctor } from './components/PrimaryCareDoctor';
+import React from 'react';
+import { MedicalInformation } from './components/MedicalInfo';
+import { Medications } from './components/Medications';
+import { Privacy } from './components/Privacy';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <div style={{ height: '100%' }}> */}
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+          <Route
+            path={appPathNames.patientInformation}
+            element={
+              <React.Suspense fallback={<div>Loading...</div>}>
+                <PatientInfo />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path={`${appPathNames.primaryCareDoctor}`}
+            element={<PrimaryCareDoctor />}
+          />
+          <Route
+            path={`${appPathNames.medicalInformation}`}
+            element={<MedicalInformation />}
+          />
+          <Route
+            path={`${appPathNames.medications}`}
+            element={<Medications />}
+          />
+          <Route
+            path={`${appPathNames.appointments}`}
+            element={<Appointments />}
+          />
+
+          <Route path={`${appPathNames.privacy}`} element={<Privacy />} />
+        </Route>
+      </Routes>
+      {/* </div> */}
+    </>
   );
-}
+};
 
 export default App;
